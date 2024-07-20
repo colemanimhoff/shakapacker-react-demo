7748480abccd6eb59e704e7028991f2fd89f7a40
Coleman Imhoff <colemanimhoff@gmail.com>
2024-07-20 17:01:53 -0600

    adds state to further prove HMR is working

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const HelloMessage = ({ name }: { name: string }) => <h1>Hello, {name}!</h1>;
+const HelloMessage = ({ name }: { name: string }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Hello, {name}!</h1>
+      <p>{count}</p>
+      <button type="button" onClick={() => setCount((prev) => prev + 1)}>
+        Count
+      </button>
+    </>
+  );
+};
 
 export default HelloMessage;
