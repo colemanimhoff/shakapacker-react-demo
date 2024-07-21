242f90235d5fb3054ab957908ddb213eacaca54c
Coleman Imhoff <colemanimhoff@gmail.com>
2024-07-21 10:29:46 -0600

    makes entrypoint a ts file

--- app/javascript/packs/App.tsx
+++ app/javascript/packs/App.tsx
@@ -7,8 +7,17 @@ const HelloMessage = ({ name }: { name: string }) => {
       <h1>Hello, {name}!</h1>
       <p>{count}</p>
       <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-        Count
+        Click Me
       </button>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis
+        mauris tortor. Fusce eu tellus eget neque vulputate varius. In sed nunc
+        nec ex mollis dictum. Duis pretium tincidunt sem ac dictum. Etiam sed
+        elit et justo dapibus aliquam ac non elit. Pellentesque habitant morbi
+        tristique senectus et netus et malesuada fames ac turpis egestas. In
+        orci orci, mattis vitae tellus sed, commodo porttitor turpis.
+      </p>
     </>
   );
 };
